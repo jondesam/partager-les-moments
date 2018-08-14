7aab6a4603ef2d8f0df1f6c3fe964e03071cad4d
jonhyun <godaiys@hotmail.com>
2018-08-14 00:03:00 -0400

    first commit

---
+++ PartagerLesMoments/PhotoController.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoController.swift
+//  Partager les moments
+//
+//  Created by MyMac on 18-08-12.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+
+    @IBOutlet weak var partagerButton: UIBarButtonItem!
+    
+    @IBOutlet weak var photoAPartager: UIImageView!
+    
+    @IBOutlet weak var texteAPartager: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    @IBAction func boutonPartageAppute(_ sender: Any) {
+    }
+    
+}
