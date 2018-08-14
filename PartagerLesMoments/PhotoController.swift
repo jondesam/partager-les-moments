7eba33b1abf312c9e5627a491b85c337a055e61d
jonhyun <godaiys@hotmail.com>
2018-08-14 00:17:38 -0400

    func miesEnPlace

--- PartagerLesMoments/PhotoController.swift
+++ PartagerLesMoments/PhotoController.swift
@@ -16,10 +16,26 @@ class PhotoController: UIViewController {
     
     @IBOutlet weak var texteAPartager: UITextView!
     
+    let texteVide = "Entrez un Texte..."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+       miseEnPlace()
+    }
+    
+    func miseEnPlace() {
+        photoAPartager.contentMode = .scaleAspectFit
+        photoAPartager.image = #imageLiteral(resourceName: "Superman-facebook.svg")
+        photoAPartager.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(prendrePhoto))
+        photoAPartager.addGestureRecognizer(tap)
+        
+        texteAPartager.text = texteVide
+    }
+    
+    @objc func prendrePhoto(){
+        
     }
 
     @IBAction func boutonPartageAppute(_ sender: Any) {
